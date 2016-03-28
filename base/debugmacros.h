00440531650a6720acacea19fbe345fac1d0555e
Dario Berzano <dario.berzano@cern.ch>
2016-03-28 19:04:57 +0200

    Avoid warnings in debug macros expansion

--- base/debugmacros.h
+++ base/debugmacros.h
@@ -28,46 +28,47 @@
 #  endif
 
 #  define DEBUGENVNAME "DEBUG_" DEBUG_TAG 
-#  define DEBUGMSG(X,MSG) if(getenv(DEBUGENVNAME)!=NULL) \
-                       if(atoi(getenv(DEBUGENVNAME)) >= X ) \
-                         if(getenv("DEBUGFILE")!=NULL) {\
-                           std::ofstream dbgstr(getenv("DEBUGFILE"),std::ios::app);\
-                           dbgstr << "(" << DEBUGENVNAME <<"=" << X \
-                                  << "," << __FUNCTION__ << "): " MSG;} \
-                         else \
-                           std::cout << "(" << DEBUGENVNAME <<"=" << X \
-                                  << "," << __FUNCTION__ << "): " MSG;
+#  define DEBUGMSG(X,MSG) if(getenv(DEBUGENVNAME)!=NULL && atoi(getenv(DEBUGENVNAME)) >= X) { \
+                            if(getenv("DEBUGFILE")!=NULL) {                                   \
+                              std::ofstream dbgstr(getenv("DEBUGFILE"),std::ios::app);        \
+                              dbgstr << "(" << DEBUGENVNAME <<"=" << X                        \
+                                     << "," << __FUNCTION__ << "): " MSG;                     \
+                            }                                                                 \
+                            else {                                                            \
+                              std::cout << "(" << DEBUGENVNAME <<"=" << X                     \
+                                     << "," << __FUNCTION__ << "): " MSG;                     \
+                            }                                                                 \
+                          }                                                                   \
 
-#  define DEBUGHEXDUMP(X,MSG,PTR,LENGTH) if(getenv(DEBUGENVNAME)!=NULL) \
-                       if(atoi(getenv(DEBUGENVNAME)) >= X ) \
-                         if(getenv("DEBUGFILE")!=NULL) { \
-                           std::ofstream dbgstr(getenv("DEBUGFILE"),std::ios::app);\
-                           dbgstr << "(" << DEBUGENVNAME <<"=" << X \
-                                  << "," << __FUNCTION__ << "): " MSG;\
-                           dbgstr.setf(std::ios::hex); \
-                           int counter=0; \
-                           for(unsigned char *debgi=(unsigned char*)PTR; \
-                               debgi<((unsigned char*) PTR+(LENGTH));debgi++){ \
-                              if(++counter%12==1) dbgstr << "\n"; \
-			      dbgstr << std::setw(4) \
-                                     << "  " << (unsigned int) *debgi; \
-                           } \
-                           dbgstr.unsetf(std::ios::hex);\
-                           dbgstr << "\n"; \
-                         } else { \
-                           std::cout << "(" << DEBUGENVNAME <<"=" << X \
-                                  << "," << __FUNCTION__ << "): " MSG;\
-                           std::cout.setf(std::ios::hex); \
-                           int counter=0; \
-                           for(unsigned char *debgi=(unsigned char*)PTR; \
-                             debgi<((unsigned char*) PTR+(LENGTH));debgi++){ \
-                              if(++counter%12==1) std::cout << "\n"; \
-			      std::cout << std::setw(4) \
-                                     << "  " << (unsigned int) *debgi; \
-                           }\
-                           std::cout.unsetf(std::ios::hex);\
-                           std::cout << "\n"; \
-                         }
+#  define DEBUGHEXDUMP(X,MSG,PTR,LENGTH) if (getenv(DEBUGENVNAME)!=NULL && atoi(getenv(DEBUGENVNAME)) >= X) {                                 \
+                                           if (getenv("DEBUGFILE")!=NULL) {                                                                   \
+                                             std::ofstream dbgstr(getenv("DEBUGFILE"),std::ios::app);                                         \
+                                             dbgstr << "(" << DEBUGENVNAME <<"=" << X                                                         \
+                                                    << "," << __FUNCTION__ << "): " MSG;                                                      \
+                                             dbgstr.setf(std::ios::hex);                                                                      \
+                                             int counter=0;                                                                                   \
+                                             for (unsigned char *debgi=(unsigned char*)PTR; debgi<((unsigned char*) PTR+(LENGTH)); debgi++) { \
+                                                if(++counter%12==1) dbgstr << "\n";                                                           \
+		                                dbgstr << std::setw(4)                                                                        \
+                                                       << "  " << (unsigned int) *debgi;                                                      \
+                                             }                                                                                                \
+                                             dbgstr.unsetf(std::ios::hex);                                                                    \
+                                             dbgstr << "\n";                                                                                  \
+                                           }                                                                                                  \
+                                           else {                                                                                             \
+                                             std::cout << "(" << DEBUGENVNAME <<"=" << X                                                      \
+                                                       << "," << __FUNCTION__ << "): " MSG;                                                   \
+                                             std::cout.setf(std::ios::hex);                                                                   \
+                                             int counter=0;                                                                                   \
+                                             for (unsigned char *debgi=(unsigned char*)PTR; debgi<((unsigned char*) PTR+(LENGTH)); debgi++) { \
+                                               if(++counter%12==1) std::cout << "\n";                                                         \
+		                               std::cout << std::setw(4)                                                                      \
+                                                         << "  " << (unsigned int) *debgi;                                                    \
+                                             }                                                                                                \
+                                             std::cout.unsetf(std::ios::hex);                                                                 \
+                                             std::cout << "\n";                                                                               \
+                                           }                                                                                                  \
+                                         }
 #else
 #  define DEBUGMSG(X,MSG)
 #  define DEBUGHEXDUMP(X,MSG,PTR,LENGTH)
